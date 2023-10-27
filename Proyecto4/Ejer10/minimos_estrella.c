d938b7a5e8825cc2d3dbb27dd3baf4a9e49b326c
EmpanadasNqn <santino123arg@gmail.com>
2023-10-27 12:30:51 -0300

    Proyecto 4, Ejercicio 10.

--- Proyecto4/Ejer10/minimos_estrella.c
+++ Proyecto4/Ejer10/minimos_estrella.c
@@ -45,6 +45,21 @@ bool todos_pares(int tam, int a[]) {
     return res;
 }
 
+bool todos_impares(int tam, int a[]) {
+    int i = 0;
+    bool res = true;
+
+    while (i < tam) {
+
+        if (a[i] % 2 == 0) {
+            res = false;
+        }
+        i = i + 1;
+    }
+    
+    return res;
+}
+
 int minimo_pares(int tam, int a[]) {
     int i = 0;
     int res = INT_MAX;
@@ -94,7 +109,7 @@ int main() {
     
     if (todos_pares(tam, a)) {
         printf("Todos los elementos del arreglo son Par.\nY el minimo es %d.\n",minPar);
-    } else if (!todos_pares(tam, a)) {
+    } else if (todos_impares(tam, a)) {
         printf("Todos los elementos del arreglo son ImPar.\nY el minimo es %d.\n",minImpar);
     } else {
         printf("El elemento Par Minimo del arreglo es %d, y el Impar Minimo es %d.\n",minPar,minImpar);
@@ -107,9 +122,9 @@ int main() {
 /*
 Ingrese el tamaño de su arreglo a :=3
 
-Ingrese, en la posicion 0 del arreglo, el entero :=6
-Ingrese, en la posicion 1 del arreglo, el entero :=2
-Ingrese, en la posicion 2 del arreglo, el entero :=8
+Ingrese, en la posicion 0 del arreglo, el entero :=4
+Ingrese, en la posicion 1 del arreglo, el entero :=8
+Ingrese, en la posicion 2 del arreglo, el entero :=2
 
 Todos los elementos del arreglo son Par.
 Y el minimo es 2.
@@ -125,19 +140,19 @@ Y el minimo es 5.
 
 Ingrese el tamaño de su arreglo a :=3
 
-Ingrese, en la posicion 0 del arreglo, el entero :=6
+Ingrese, en la posicion 0 del arreglo, el entero :=4
 Ingrese, en la posicion 1 del arreglo, el entero :=7
-Ingrese, en la posicion 2 del arreglo, el entero :=1
+Ingrese, en la posicion 2 del arreglo, el entero :=9
 
-Todos los elementos del arreglo son ImPar.
-Y el minimo es 1.
+El elemento Par Minimo del arreglo es 4, y el Impar Minimo es 7.
+Y el elemento minimo del arreglo es 4.
 
 Ingrese el tamaño de su arreglo a :=3
 
-Ingrese, en la posicion 0 del arreglo, el entero :=8
+Ingrese, en la posicion 0 del arreglo, el entero :=9
 Ingrese, en la posicion 1 del arreglo, el entero :=10
-Ingrese, en la posicion 2 del arreglo, el entero :=4
+Ingrese, en la posicion 2 del arreglo, el entero :=11
 
-Todos los elementos del arreglo son Par.
-Y el minimo es 4.
+El elemento Par Minimo del arreglo es 10, y el Impar Minimo es 9.
+Y el elemento minimo del arreglo es 9.
 */
