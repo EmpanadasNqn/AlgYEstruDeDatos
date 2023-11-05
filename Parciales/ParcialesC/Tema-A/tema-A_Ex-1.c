86106ef36d968d5b42a33e603403b39c95232981
EmpanadasNqn <santino123arg@gmail.com>
2023-11-04 23:59:49 -0300

    Parciales Practica

---
+++ Parciales/ParcialesC/Tema-A/tema-A_Ex-1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <assert.h>
+
+int pedir_entero(char n) {
+    int x;
+    printf("Ingrese un entero en %c = ",n);
+    scanf("%d",&x);
+    return x;
+}
+
+void mostrar_entero(char n, int x) {
+    printf("El entero %c es %d.\n",n,x);
+}
+
+int main() {
+    int r, R,s, S;
+    
+    //Ingreso los valores de r y s que son los mismos valores de R y S.
+    r = pedir_entero('r');
+    s = pedir_entero('s');
+    R = r;
+    S = s;
+
+    assert(r == R && s == S && S > R);  //PreCondicion
+
+    r = s - r;
+    s = r + s;
+
+    assert(r == S - R && s == R + S);   //PostCondicion
+
+    //Muestro los valores de r y s.
+    mostrar_entero('r',r);
+    mostrar_entero('s',s);
+
+    return 0;
+}
